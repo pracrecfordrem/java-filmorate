b508f6be60f4205f61f8ffec4e51db7367bee238
recfordrem <rbarinov@diasoft.ru>
2024-06-27 14:11:45 +0300

    Init2

--- java-filmorate/src/main/java/ru/yandex/practicum/controller/UserController.java
+++ java-filmorate/src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         } else if (user.getName() == null) {
             user.setName(user.getLogin());
-        } else if (user.getBirthday().isAfter(LocalDate.of(Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH),Calendar.getInstance().get(Calendar.DAY_OF_MONTH) ))) {
+        } else if (user.getBirthday().isAfter(LocalDate.of(Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH),Calendar.getInstance().get(Calendar.DAY_OF_MONTH)))) {
             throw new ValidationException("Дата рождения не может быть в будущем");
         }
         if (user.getId() == null) {
@@ -53,7 +53,7 @@ public class UserController {
             throw new ValidationException("Логин не может быть пустым и содержать пробелы");
         } else if (user.getName() == null) {
             user.setName(user.getLogin());
-        } else if (user.getBirthday().isAfter(LocalDate.of(Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH),Calendar.getInstance().get(Calendar.DAY_OF_MONTH) ))) {
+        } else if (user.getBirthday().isAfter(LocalDate.of(Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH),Calendar.getInstance().get(Calendar.DAY_OF_MONTH)))) {
             throw new ValidationException("Дата рождения не может быть в будущем");
         } else if (!users.containsKey(user.getId()) || user.getId() == null) {
             throw new ValidationException("ИД изменямого пользователя не может быть равен нулю");
