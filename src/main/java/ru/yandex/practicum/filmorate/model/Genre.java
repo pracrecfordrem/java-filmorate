9578faf3ea9db5a909a416c45faa1f86bcbef7a5
recfordrem <rbarinov@diasoft.ru>
2024-08-12 14:12:41 +0300

    First

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -3,12 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Data;
 
 @Data
-public class Genre implements Comparable<Genre>{
+public class Genre implements Comparable<Genre> {
     Long id;
     String name;
 
     @Override
     public int compareTo(Genre o) {
-        return Integer.parseInt(String.valueOf(this.id-o.getId()));
+        return Integer.parseInt(String.valueOf(this.id - o.getId()));
     }
 }
